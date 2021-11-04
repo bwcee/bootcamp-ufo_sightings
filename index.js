dc10b7559b06f2b986059f49da8b2183cacb07e8
bw <bwceemail@gmail.com>
2021-11-04 17:05:08 +0800

    added full_sighting page and did a bit of submit

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 import express from "express";
-import { read, add } from "./json_arw.js";
-import methodOverride from 'method-override';
+import { read, add, edit, remove } from "./json_arw.js";
+import methodOverride from "method-override";
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 const goHome = (request, response) => {
-  read('data.json', (err, data) => {
+  read("data.json", (err, data) => {
     const sightingsArr = data.sightings;
     if (err) {
       response.status(404).send("Git outta here, there's no such page!");
     }
-    response.render('pages/home', { sightingsArr });
+    response.render("pages/home", { sightingsArr });
   });
-  
+};
+
+const showFullSighting = (request, response) => {
+  const { index } = request.params;
+  read("data.json", (err, data) => {
+    const thisSighting = data.sightings[index];
+    if (err) {
+      response.status(404).send("Git outta here, there's no such page!");
+    }
+    response.render("pages/full_sighting", { thisSighting });
+  });
 };
 
 const submitSighting = (request, response) => {
-  response.render("submit_form");
+  response.render("pages/submit_form");
 };
 
-app.post("/sighted", (request, response) => {
-  // Add new recipe data in request.body to recipes array in data.json.
+app.post("/sighting", (request, response) => {
   const newSighting = request.body;
   add("data.json", "sightings", newSighting, (err) => {
     if (err) {
@@ -36,18 +45,20 @@ app.post("/sighted", (request, response) => {
   });
 });
 
-app.delete('/sighting/delete/:index', (request, response) => {
-  // Remove element from DB at given index
-  const { index } = request.params;
-  read('data.json', (err, data) => {
-    data['recipes'].splice(index, 1);
-    write('data.json', data, (err) => {
-      response.send('Done!');
-    });
+app.delete("/sighting/delete/:index", (request, response) => {
+  // { index } is a destructuring method
+  const { indexToRemove } = request.params;
+  remove("data.json", "sightings", indexToRemove, (err) => {
+    if (err) {
+      response.send("Not able to delete!");
+      return;
+    }
+    response.redirect(303, "/");
   });
 });
 
-app.get("/sighting", submitSighting);
 app.get("/", goHome);
+app.get("/sighting/:index", showFullSighting);
+app.get("/sighting", submitSighting);
 
 app.listen(3004);
--- json_arw.js
+++ json_arw.js
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs';
+import { readFile, writeFile } from "fs";
 
 function write(filename, jsonContentObj, funcInWrite) {
   // eslint-disable-next-line max-len
@@ -8,21 +8,21 @@ function write(filename, jsonContentObj, funcInWrite) {
   // Write content to DB
   writeFile(filename, jsonContentStr, (writeErr) => {
     if (writeErr) {
-      console.error('Write error', jsonContentStr, writeErr);
+      console.error("Write error", jsonContentStr, writeErr);
       // Allow each client app to handle errors in their own way
       funcInWrite(writeErr, null);
       return;
     }
-    console.log('Write success!');
+    console.log("Write success!");
     // Call client-provided callback on successful write
     funcInWrite(null, jsonContentObj);
   });
 }
 
 function read(filename, funcInRead) {
-  readFile(filename, 'utf-8', (readErr, jsonContentStr) => {
+  readFile(filename, "utf-8", (readErr, jsonContentStr) => {
     if (readErr) {
-      console.error('Read error', readErr);
+      console.error("Read error", readErr);
       // Allow client to handle error in their own way
       return funcInRead(readErr, null);
     }
@@ -38,7 +38,7 @@ function edit(filename, func1InEdit, func2InEdit) {
   read(filename, (readErr, jsonContentObj) => {
     // Exit if there was a read error
     if (readErr) {
-      console.error('Read error', readErr);
+      console.error("Read error", readErr);
       func1InEdit(readErr, null);
       return;
     }
@@ -61,78 +61,70 @@ function add(filename, key, input, callPrintFunc) {
     (err, jsonContentObj) => {
       // Exit if there was an error
       if (err) {
-        console.error('Edit error', err);
+        console.error("Edit error", err);
         callPrintFunc(err);
         return;
       }
 
       // Exit if key does not exist in DB
       if (!(key in jsonContentObj)) {
-        console.error('Key does not exist');
+        console.error("Key does not exist");
         // Call callback with relevant error message to let client handle
-        callPrintFunc('Key does not exist');
+        callPrintFunc("Key does not exist");
         return;
       }
       // Add input element to target array
       jsonContentObj[key].push(input);
     },
     // this is passed back to edit() as argument for func2InEdit parameter
-    callPrintFunc,
+    callPrintFunc
   );
 }
 
 function remove(filename, key, index, callPrintFunc) {
-  if (process.argv.length < 5) {
-    console.log(
-      'Please input command as: "node index.js remove <list: either items or done, to remove from> <number of the item to remove>"',
-    );
-  } else {
-    edit(
-      filename,
-      (err, jsonContentObj) => {
-        if (err) {
-          console.error('Edit error', err);
-          callPrintFunc(err);
-          return;
-        }
-        if (!(key in jsonContentObj)) {
-          console.error('Key does not exist');
-          callPrintFunc('Key does not exist');
-          return;
-        }
-        jsonContentObj[key].splice(index, 1);
-      },
-      callPrintFunc,
-    );
-  }
+  edit(
+    filename,
+    (err, jsonContentObj) => {
+      if (err) {
+        console.error("Edit error", err);
+        callPrintFunc(err);
+        return;
+      }
+      if (!(key in jsonContentObj)) {
+        console.error("Key does not exist");
+        callPrintFunc("Key does not exist");
+        return;
+      }
+      jsonContentObj[key].splice(index, 1);
+    },
+    callPrintFunc
+  );
 }
 
 function editOneElement(filename, key, index, payload, callPrintFunc) {
   if (process.argv.length < 6) {
     console.log(
-      'Please input command as: "node index.js editOne <list: either items or done> <number of the item to edit> <contents to change item to>"',
+      'Please input command as: "node index.js editOne <list: either items or done> <number of the item to edit> <contents to change item to>"'
     );
   } else {
     edit(
       filename,
       (err, jsonContentObj) => {
         if (err) {
-          console.error('Edit error', err);
+          console.error("Edit error", err);
           callPrintFunc(err);
           return;
         }
         if (!(key in jsonContentObj)) {
-          console.error('Key does not exist');
-          callPrintFunc('Key does not exist');
+          console.error("Key does not exist");
+          callPrintFunc("Key does not exist");
           return;
         }
         jsonContentObj[key][index] = payload;
       },
-      callPrintFunc,
+      callPrintFunc
     );
   }
 }
 
-export {
-  write, read, add, edit, remove, editOneElement,
-};
+export { write, read, add, edit, remove, editOneElement };
